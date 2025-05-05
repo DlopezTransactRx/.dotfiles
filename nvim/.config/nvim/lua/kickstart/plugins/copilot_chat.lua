438f29aef9b9445294a0420422c43ffa34ba647b
DlopezTransactRx <dlopez@transactrx.com>
2025-05-05 10:02:02 -0400

    Add CopilotChat Keymap

--- nvim/.config/nvim/lua/kickstart/plugins/copilot_chat.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/copilot_chat.lua
@@ -1,14 +1,23 @@
 return {
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
+    'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
+    build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
-      -- See Configuration section for options
+      panel = { enabled = true }, -- Enable the CopilotChat panel
+      suggestion = { enabled = true }, -- Enable suggestions
     },
-    -- See Commands section for default commands if you want to lazy load on them
+    config = function(_, opts)
+      require('CopilotChat').setup(opts)
+
+      -- Keymap to toggle CopilotChat
+      vim.keymap.set('n', '<leader>cc', ':CopilotChat<CR>', { noremap = true, silent = true })
+
+      -- Keymap to accept autocomplete suggestions
+      -- vim.keymap.set('i', '<C-l>', 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
+    end,
   },
 }
