e5d83c7544d7813315e79a5e5c4e9874ac9d1ed6
DlopezTransactRx <dlopez@transactrx.com>
2025-05-13 21:35:48 -0400

    Enable debug, lint, and index_line plugins.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1002,9 +1002,9 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.copilot', -- Removed in favor of github/copilot.vim
   require 'kickstart.plugins.copilot_chat',
@@ -1026,13 +1026,10 @@ require('lazy').setup({
   require 'kickstart.plugins.outline',
   require 'kickstart.plugins.headlines',
 
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
