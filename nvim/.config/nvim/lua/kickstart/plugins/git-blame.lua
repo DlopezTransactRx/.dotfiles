d1dd52bbece8ce03680204024219f320643a2f84
DlopezTransactRx <dlopez@transactrx.com>
2025-06-27 01:25:41 -0400

    Toggle Git Blame Option.  Default False.

--- nvim/.config/nvim/lua/kickstart/plugins/git-blame.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/git-blame.lua
@@ -9,7 +9,7 @@ return {
   opts = {
     -- your configuration comes here
     -- for example
-    enabled = true, -- if you want to enable the plugin
+    enabled = false, -- if you want to enable the plugin
     message_template = ' <summary> • <date> • <author> • <<sha>>', -- template for the blame message, check the Message template section for more options
     date_format = '%m-%d-%Y %H:%M:%S', -- template for the date, check Date format section for more options
     virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
@@ -24,6 +24,9 @@ return {
     }),
     vim.keymap.set('n', '<leader>gu', ':GitBlameCopyFileURL<cr>', {
       desc = 'Copy File URL',
+    }),
+    vim.keymap.set('n', '<leader>gt', ':GitBlameToggle<cr>', {
+      desc = 'Toggle Git Blame',
     }),
   },
 }
